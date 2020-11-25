443a9245af5688391a385ab23b4a0c4e836df321
Juan Carlos Agreda <51544423+juancarlosagreda@users.noreply.github.com>
2020-11-26 00:56:52 +0100

    Add files via upload

---
+++ Parcial Regresion/parcialML.R
@@ -0,0 +1,19 @@
+#Parcial regresion
+#By Juan Carlos Agreda
+
+#####Paquetes#####
+#Son todos los paquetes que hemos usado hasta ahora
+library(MASS)
+library(ISLR)
+library(psych)
+library(caret)
+library(caret)
+library (ggplot2)
+library(DMwR)
+library(stats)
+library(corrplot)
+library(plyr)
+library(magrittr)
+
+#####Ejercicio 1#####
+
